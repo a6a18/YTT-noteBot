eb0f7e871922803b614ff5cdfea25a429bf3433e
Tom Shen <tom.shen@tgc-taiwan.com.tw>
2020-03-22 18:38:17 +0800

    save linebot link

--- app.py
+++ app.py
@@ -6,12 +6,15 @@ from linebot.exceptions import (
     InvalidSignatureError
 )
 from linebot.models import *
-from flask import Flask
+from flask import Flask, request, abort
 
 app = Flask(__name__)
 line_bot_api = LineBotApi(
-    'oPYz4BpH7Ku0wwnpHvneWmHdCrCpY3Vy6LPYRBnHJNIME0MY1YC2KLmgQT3b/XwG2M0+g3tteIDxVeVZQoRaZIKA2+eadmvRlbM0uiAotkFC6yek811Ayl87eeHa0RYwIp3CJfYHYoGp36+nYWeeSgdB04t89/1O/w1cDnyilFU=')
-handler = WebhookHandler('05f3de3f167958ca435d64a6032558f2')
+    'f3DH5PI/pP+u+KQbqDCXr9G3ydwq6GVEsDQOMuilVh6/geI1j+MrPnRdxoLcSH+LEBcMmhAGTptPg7moP2rZx9Dxg1MMGbOGyqwhm3FutumcNDFkvcnYr/amps46hhmV6KUW/vLuHSCXT2KKEEJ9hwdB04t89/1O/w1cDnyilFU=')
+handler = WebhookHandler('ac24d1d12114ff7d1da90f2864516da0')
+
+
+
 
 
 ####### 資料庫 ######
@@ -20,6 +23,23 @@ myclient = pymongo.MongoClient("mongodb+srv://a6a18:Aa19950501@cluster0-8ingu.mo
 
 
 ###### 規則 ######
+
+
+# 監聽所有來自 /callback 的 Post Request
+@app.route("/callback", methods=['POST'])
+def callback():
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+    return 'OK'
+
 
 def save_link(category, link):
     mydb = myclient['Line']
